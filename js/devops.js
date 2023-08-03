82b36263cee886d082a07a83cad31427f450d0d0
armstrongja <jarearms@iu.edu>
2023-08-02 22:44:35 -0400

    Initial Commit

---
+++ js/devops.js
@@ -0,0 +1,36 @@
+fetch('https://api-dot-di-dashboard-lids.uc.r.appspot.com/api/v1/devops')
+.then(response => response.json())
+.then(allTitles => {
+    renderData(allTitles);
+    console.log(allTitles)
+});
+
+
+const cardsContainer = document.querySelector('#cards-container');
+
+function renderData(pages) {
+    pages.forEach(page => {
+      // create variables for all elements within the card
+      const url = document.createElement('a');
+      const cardColumn = document.createElement('div');
+      const cardInfo = document.createElement('div');
+      const title = document.createElement('h2');
+
+      // create classes for all elements
+      cardColumn.classList = 'card col';
+      title.classList = 'card-title'
+
+      // data pulled for each state
+      title.innerText = page.name;
+      url.href = page.url;
+
+      url.target = '_blank'
+
+      // used appendChild to make sure all elements are inside the cardColumn div and in cardsContiner id
+      url.appendChild(cardColumn)
+      cardColumn.appendChild(cardInfo);
+      cardInfo.appendChild(title);
+      cardsContainer.appendChild(url);
+    
+    });
+}
